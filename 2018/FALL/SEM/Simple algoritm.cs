72a18246f36d7f7c01fe581bd9e7b65957f5580f
Farit <farit.shamardanov@outlook.com>
2018-09-11 01:14:59 +0300

    Firs commit

---
+++ 2018/FALL/SEM/Simple algoritm.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace Experiment
+{
+    class MainClass
+    {
+        //метод для 6 задачи
+     /*   public static double SerchDistans(int x1, int y1, int x2, int y2)
+        {
+            double a = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            return a;
+        }   */
+        public static void Main(string[] args)
+        {
+
+
+            /* Вторая задача */
+            /*  //Read stroka
+              string str = Console.ReadLine();
+              int number = int.Parse(str);
+              //сотня
+              int sto = number / 100;
+              //еденицы
+              int ten = number % 10;
+              //десятки
+              int center = (number / 10) % 10;  
+              //преобразование чисел в строку
+              string newStr1 = sto.ToString();
+              string newStr2 = ten.ToString();
+              string newStr3 = center.ToString();
+              //вывод
+              Console.WriteLine(newStr2 + newStr3 + newStr1);
+              */
+
+
+
+
+
+            /* Третья задача */
+            /*
+            string str = Console.ReadLine();
+            int clock = int.Parse(str);
+            int grad = 0;
+            if (clock < 12 && clock > 6)
+            {
+                grad = 360 - clock * 30;
+            }
+            else
+            {
+                if (clock > 12)
+                {
+                    grad = ((24 - clock) * 30);
+                }
+
+                else {
+                    grad = clock * 30;
+                }
+            }
+            Console.WriteLine(grad);
+            */
+
+
+
+
+            /* Четвертая задача */
+            /*      int n = int.Parse(Console.ReadLine());
+                  int x = int.Parse(Console.ReadLine());
+                  int y = int.Parse(Console.ReadLine());
+                  Console.WriteLine(((n - 1) / x) + ((n - 1) / y) - ((n - 1) / (x + y)));
+                  */
+
+
+
+            /*Пятая задача*/
+            /*  int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                Console.WriteLine((b / 4 - b / 100 + b / 400) - (a / 4 - a / 100 + a / 400));
+
+            */
+
+
+
+            /*Шестая задача (нужно подключить метод) */
+            /*  Console.WriteLine("Enter x1");
+              int x1 = int.Parse(Console.ReadLine());
+              Console.WriteLine("Enter y1");
+              int y1 = int.Parse(Console.ReadLine());
+              Console.WriteLine("Enter x2");
+              int x2 = int.Parse(Console.ReadLine());
+              Console.WriteLine("Enter y2");
+              int y2 = int.Parse(Console.ReadLine());
+              Console.WriteLine("Ener point cordinat x");
+              int x = int.Parse(Console.ReadLine());
+              Console.WriteLine("Ener point cordinat y");
+              int y = int.Parse(Console.ReadLine());
+              double a = SerchDistans(x1, y1, x2, y2);
+              double b = SerchDistans(x1, y1, x, y);
+              double c = SerchDistans(x, y, x2, y2);
+              double p = (a + b + c) / 2;
+              double h = (2 / a) * Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+              Console.WriteLine("Ditans is " + h);
+              */
+
+
+
+
+
+            /*Седьмая задача*/
+            /*  Console.WriteLine("Equation of the line is Ax + By +C = 0");
+              Console.WriteLine("Enter A");
+              int A = int.Parse(Console.ReadLine());
+              Console.WriteLine("Enter B");
+              int B = int.Parse(Console.ReadLine());
+              Console.WriteLine("Enter C");
+              int C = int.Parse(Console.ReadLine());
+              Console.WriteLine("Perpendicular vektor is p({0},{1})", A,B);
+              Console.WriteLine("Parallel vektor is p({0},{1})", -B, A);
+              */
+
+
+
+
+
+            //задаваться будет равнение прямой вида Ax+By+c=0 и кордината точки
+            Console.WriteLine("Enter A");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter B");
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter C");
+            int C = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter x");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter y");
+            int y = int.Parse(Console.ReadLine());
+            int C2 = -B * x + A * y;
+            double y2 = (C2 * A - C * B) / (A * A + B * B);
+            double x2 = (-C - B * y) / A;
+            Console.WriteLine("({0},{1})", x2, y2);
+
+        }
+        
+    }
+}
