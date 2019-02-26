d53fb9e2ad00723cd310beeb4648f61d429470a1
farit2000 <42946448+farit2000@users.noreply.github.com>
2019-02-27 02:08:33 +0300

    Add files via upload
    Compare SmoothSort Array vs LinkedList

---
+++ 2018/FALL/Graph/Graph/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Graph
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DrawGraph();
+        }
+
+        private void DrawGraph()
+        {
+            SmoothSort smoothsort = new SmoothSort();
+            StreamReader sr = new StreamReader("C:/Users/farit/Documents/Projects/Graph/mas.txt");
+            var rnd = new Random();
+            string line;
+            var watch = new Stopwatch();
+            PointPairList time1 = new PointPairList();
+            PointPairList time2 = new PointPairList();
+            GraphPane pane = zedGraphControl1.GraphPane;
+
+            pane.Title = "Array vs LinkedList";
+            pane.XAxis.Title = "Data weight";
+            pane.YAxis.Title = "Tik numbers";
+            pane.CurveList.Clear();
+            PointPairList f1_list = new PointPairList();
+            PointPairList f2_list = new PointPairList();
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                var test = line.Split().Select(int.Parse).ToArray();
+                var linkedList = new LinkedList<int>();
+                for (int i = 0; i < test.Length; i++)
+                {
+                    linkedList.AddLast(test[i]);
+                }
+                watch.Stop();
+                watch.Restart();
+                smoothsort.Sort(test);
+                watch.Stop();
+                time1.Add(test.Length, watch.ElapsedTicks);
+
+                watch.Stop();
+                watch.Restart();
+                smoothsort.Sort(linkedList);
+                watch.Stop();
+                time2.Add(linkedList.Count, watch.ElapsedTicks);
+            }
+                LineItem f1_curve = pane.AddCurve("Array", time1, Color.Blue, SymbolType.Circle);
+                LineItem f2_curve = pane.AddCurve("Lin", time2, Color.Red, SymbolType.Plus);
+                zedGraphControl1.AxisChange();
+                zedGraphControl1.Invalidate();
+        }
+
+        private void Setsize()
+        {
+            zedGraphControl1.Location = new Point(10,10);
+
+            zedGraphControl1.Size = new Size(this.ClientRectangle.Width - 20, this.ClientRectangle.Height - 20);
+        }
+    }
+}
---
+++ 2018/FALL/Graph/Graph/SmoothSort.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Graph
+{
+    internal class SmoothSort
+    {
+        //input array to sort
+        public void Sort(int[] InputArray)
+        {
+            int len = InputArray.Length;
+            for (int i = len / 2 - 1; i >= 0; i--)
+                LeonardHeap(InputArray, len, i);
+            //create binary Heap
+            for (int i = len - 1; i >= 0; i--)
+            {
+                int temp = InputArray[0];
+                InputArray[0] = InputArray[i];
+                InputArray[i] = temp;
+                LeonardHeap(InputArray, i, 0);
+            }
+        }
+
+        public void Sort(LinkedList<int> linkedList)
+        {
+            var InputArray = linkedList.ToArray();
+            int len = InputArray.Length;
+            for (int i = len / 2 - 1; i >= 0; i--)
+                LeonardHeap(InputArray, len, i);
+            //create binary Heap
+            for (int i = len - 1; i >= 0; i--)
+            {
+                int temp = InputArray[0];
+                InputArray[0] = InputArray[i];
+                InputArray[i] = temp;
+                LeonardHeap(InputArray, i, 0);
+            }
+        }
+
+        //to compare the childs with root to make the max-heap
+        void LeonardHeap(int[] arr, int len, int index)
+        {
+            int lar = index;
+            int l = 2 * index + 1;
+            int r = 2 * index + 2;
+            if (l < len && arr[l] > arr[lar])
+                lar = l;
+            if (r < len && arr[r] > arr[lar])
+                lar = r;
+            if (lar != index)
+            {
+                int swap = arr[index];
+                arr[index] = arr[lar];
+                arr[lar] = swap;
+                LeonardHeap(arr, len, lar);
+            }
+        }
+
+        static void PrintSortedArray(int[] array)
+        {
+            int len = array.Length;
+            for (int i = 0; i < len; ++i)
+                Console.WriteLine(array[i] + "");
+            Console.Read();
+        }
+    }
+}
