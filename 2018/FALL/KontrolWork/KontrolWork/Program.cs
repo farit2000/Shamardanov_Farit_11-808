a29afe5e388a3f2d50734fb7cca80e495c8652fb
Farit <farit.shamardanov@outlook.com>
2019-05-15 13:10:38 +0300

    KontrolWorkLastVersion

--- 2018/FALL/KontrolWork/KontrolWork/Program.cs
+++ 2018/FALL/KontrolWork/KontrolWork/Program.cs
@@ -2,46 +2,75 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
+using.System.Threading.Tasks;
+using System.Threading.Tasks;
 
 namespace KontrolWork
 {
-    class Test
-    {
-        public int TestMethod(List<Parametr> list)
-        {
-            return list.FirstOrDefault().Value;
-        }
-    }
-
-    class Student
-    {
-        public string Name { get; set; }
-        public int TrueAnswer { get; set; }
-    }
-
-    class Parametr
-    {
-        public string Name { get; set; }
-        public int Value { get; set; }
-    }
-
-    class Program
-    {
-        Student Student { get; set; }
-        string ClassName { get; set; }
-        string MethodName { get; set; }
-        List<Parametr> Parametrs { get; set; }
-
-        public Program(string ClassName, string MethodName, List<Parametr> parametrs)
-        {
-            this.StudentName = StudentName;
-            this.ClassName = ClassName;
-            this.MethodName = MethodName;
-            this.Parametrs = parametrs;
-        }
-    }
     class MainClass
     {
+        class Studend
+        {
+            public bool check;
+            public List<Execute> Methods;
+            public string Path;
+            public Studend(string path)
+            {
+                Path = path;
+                ParseStudent(path);
+            }
+            public void ParseStudent(string path)
+            {
+                Methods = Directory.GetFiles(path).Select(f => File.ReadAllLines(f))
+               .Select(t =>
+               {
+                   return new Execute(t[1], t[0], t[2], t[3]);
+               }).ToList();
+            }
+        }
+
+        class Execute
+        {
+            public bool check;
+            public string MethodName;
+            public string ClassName;
+
+            public string arguments;
+            public string response;
+            public Execute(string methodName, string className, string args, string response)
+            {
+                this.MethodName = methodName;
+                this.ClassName = className;
+                this.arguments = args;
+                this.response = response;
+            }//type {value} responseType {value}
+            static object[] getArgs(string text)
+            {
+                var mathes = new Regex(@"{+\w*}+").Matches(text);
+                var result = new List<object>();
+                foreach (Match e in mathes)
+                {
+                    result.Add(e.Value.Substring(1, e.Value.Length - 2));
+                }
+                return result.ToArray();
+            }
+
+            public async void CheckMethod()
+            {
+                var type = Type.GetType(ClassName);
+                var method  = type.GetMethod(MethodName);
+                var constructor = type.GetConstructor(new Type[] { });
+                var result = constructor.Invoke(null);
+                var args = getArgs(arguments); 
+
+                var res = method.Invoke((object)result, args);
+                var responseType = Type.GetType(response.Split('{','}')[0]);
+                var responseValue = int.Parse(response.Split('{', '}')[1]);
+                check = (int)res == responseValue;
+            }
+        }
+
         public static void Main(string[] args)
         {
             //задача 1.1
@@ -61,35 +90,23 @@ namespace KontrolWork
                     .ToList());
             }
             */
-            string path = @"Users/faritsamardanov/Documents/Shamardanov_Farit_11-808/2018/FALL/KontrolWork/KontrolWork/Students/Root";
-            DirectoryInfo dir = new DirectoryInfo(path);
-            var dirs = Directory.GetDirectories(path);
-            foreach(var item in dirs)
+            var students = Directory.GetDirectories(@"путь до файла").Select(f => new Studend(f)).ToList();
+            foreach(var student in students)
             {
-                var files = Directory.GetFiles(item);
-                foreach(var file in files)
+                foreach(var e in student.Methods)
                 {
-                    List<Student> students = new List<Student>();
-                    students.Add(new St)
+                    Task.Run(() => e.CheckMethod());
                 }
+            }
+            //вывод таблицы
+            var arrayTrueAnswer = students.Select(s => s.Methods.Where(f => f.check).Count()).ToArray();
+            var i = 0;
+            foreach(var student in students)
+            {
+                Console.WriteLine(student.Path + ": " + arrayTrueAnswer[i]);
+                i++;
             }
         }
 
-        public Program Check(string str)
-        {
-            Student student = new Student();
-            student.Name = null;
-            string ClassName = null;
-            string MethodName = null;
-            List<Parametr> list = new List<Parametr>();
-            // здесь получаем все поля прописанные выше
-            if(list == Tr)
-            return new Program(ClassName, MethodName, list); 
-        }
-
-        public List<int> TrueMethod()
-        {
-            
-        }
     }
 }
---
+++ 2018/FALL/KontrolWork/Parser/Parser/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Parser
+{
+    class Comment
+    {
+        public int postId { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string body { get; set; }
+    }
+
+    class Answer
+    {
+        public int Id;
+        public int count;
+        public Answer(int id, int count)
+        {
+            this.Id = id;
+            this.count = count;
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var comments = new List<Comment>();
+            comments = GetComments();
+            while (comments.Any())
+            {
+                var result = comments.Distinct().ToList();
+                result.ForEach(item => comments.Remove(item));
+                WriteAsync(result);
+            }
+        }
+
+        public static async void WriteAsync(List<Comment> result)
+        {
+            Console.WriteLine("Start");
+            await Task.Run(() => Count(result));
+            Console.WriteLine("Stop");
+        }
+
+        private static void Count(List<Comment> comments)
+        {
+            List<Answer> list = new List<Answer>();
+
+            foreach (var e in comments)
+            {
+                if (e.id % 2 == 0)
+                {
+                    string result = Regex.Replace(e.body, @"[^A-Za-z]", "");
+                    list.Add(new Answer(e.id, result.Length));
+                }
+            }
+            foreach(var e in list)
+            {
+                Console.WriteLine(e.Id.ToString() + ": " + e.count);
+            }
+        }
+
+        public static List<Comment> GetComments()
+        {
+            var comments1 = JsonConvert.DeserializeObject<List<Comment>>(File.ReadAllText(@"/Users/faritsamardanov/Documents/Shamardanov_Farit_11-808/2018/FALL/KontrolWork/Parser/Parser/EmptyJSONFile.json"));
+            return comments1;
+        }
+    }
+}
